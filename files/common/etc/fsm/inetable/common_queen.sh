65c39ba8336b9efc1d76350ae27a18da6d51d5f6
Katze Miau <miaukatzemiau@priveasy.de>
2011-12-11 16:45:08 +0000

    fsm inetable: change httpd instances in queen/ghost/robinson states
    - update the redirection uhttpd config and www-root to redirect all
      incoming request to a url specified by /tmp/redirection_target
    - use this to implement robinson and splash redirection during
      robinson and queen/ghost states, respectively
    - change the service httpd to listen on the public IPv4 address
      available on the mesh interface in those states.
    
    Missing is the
    - iptable based redirection
    - already referenced /sbin/urlencode

--- files/common/etc/fsm/inetable/common_queen.sh
+++ files/common/etc/fsm/inetable/common_queen.sh
@@ -7,6 +7,7 @@ DHCPLeaseTime=$((12 * 3600))
 NodeId="$(cat /etc/nodeid)"
 oct3=$(ifconfig br-mesh | egrep -o 'inet addr:[0-9.]*'|cut -f3 -d.)
 [ -n "$oct3" ]
+gwip=10.17.$oct3.1
 
 we_own_our_ip () {
     [ "$(p2ptbl get $gwiptbl $oct3 | cut -sf2)" == "$NodeId" ]
