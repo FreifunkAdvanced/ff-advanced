ed254615ad04befe9f847879b3d78567c51f673b
Katze Miau <miaukatzemiau@priveasy.de>
2011-12-11 13:32:54 +0000

    fsm inetable: sync mesh iface w/ uci, use batctl gw off, reduce redundancy
    - add mesh_add/del_ipv4 to set/unset IPv4 addresses of mesh iface; the
      address is set using ifconfig to avoid ifudown+ifup but also updated
      in UCI to maintain a consistent state
    - use this function in queen, ghost and robinson state transitions
    - use `batctl gw off` in robinson state: this allows to deliever local
      DHCP replies while dropping the idea of announcing fake gateways
    - ghost.leave reuses queen.leave

--- files/common/etc/fsm/inetable/common.sh
+++ files/common/etc/fsm/inetable/common.sh
@@ -1,5 +1,25 @@
 cloud_is_online () {
     # look for mac addrs in batman gateway list
-    # TODO: remove fake gws with almost zero bandwith
     batctl gwl | tail -n-1 | egrep -q '([0-9a-f]{2}:){5}[0-9a-f]{2}'
+}
+
+## add/remove IPv4 address from mesh iface
+# manual update to avoid full ifdown+ifup, but update uci state for
+# other users (e.g. dnsmasq)
+mesh_add_ipv4 () {
+    ifconfig br-mesh $1 netmask $2
+    echo "
+set network.mesh.ipaddr=$1
+set network.mesh.proto=static
+set network.mesh.netmask=$2
+" | uci batch
+}
+
+mesh_del_ipv4 () {
+    ifconfig br-mesh 0.0.0.0
+    echo "
+delete network.mesh.ipaddr
+delete network.mesh.proto
+delete network.mesh.netmask
+" | uci batch
 }
