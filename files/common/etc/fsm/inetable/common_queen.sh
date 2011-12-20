754907fd31da80d247432d02128bb4e9c79d31b0
Katze Miau <miaukatzemiau@priveasy.de>
2011-12-20 19:41:26 +0000

    remove all hardcoded references to FFJ IP range, use /etc/config/cloud instead
    Replace the last bits of hardcoded FFJ IP range outside /etc/config -
    residing in fsm inetable state transitions - with IP ranges calculated
    from /etc/config/cloud.

--- files/common/etc/fsm/inetable/common_queen.sh
+++ files/common/etc/fsm/inetable/common_queen.sh
@@ -7,7 +7,8 @@ DHCPLeaseTime=$((12 * 3600))
 NodeId="$(cat /etc/nodeid)"
 oct3=$(ifconfig br-mesh | egrep -o 'inet addr:[0-9.]*'|cut -f3 -d.)
 [ -n "$oct3" ]
-gwip=10.17.$oct3.1
+gwip=$(ipcalc.sh "$(uci get cloud.cur.net_mesh)" $(($oct3 * 256 + 1)) 1 \
+     | grep ^START | cut -f2 -d=)
 
 we_own_our_ip () {
     [ "$(p2ptbl get $gwiptbl $oct3 | cut -sf2)" == "$NodeId" ]
