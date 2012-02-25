d2e697fc663ab27d0155844cf4dfa19ba0bf3d82
Tristan Leo <mail@tristan-leo.de>
2012-02-25 22:57:11 +0100

    mocked simple login view

--- files/common/www/webadmin/js/script.js
+++ files/common/www/webadmin/js/script.js
@@ -1,8 +1,73 @@
-/* Author:
+$(function () {
 
-*/
+  var render = function render ($view) {
+    var $main = $("#main");
 
+    $main.html($view);
+  };
 
+  var LoginModel = Backbone.Model.extend({
+    login: function () {
+      //TODO: Login
+      console.log("logging in");
+    }
+  });
 
+  var LoginFormView = Backbone.View.extend({
 
+    tagName: "form",
 
+    className: "login-form",
+
+    events: {
+      "click #login": "login"
+    },
+
+    render: function () {
+      this.$el
+        .append($("<input/>")
+          .attr("type", "text")
+          .attr("name", "user")
+          .attr("id", "user"))
+        .append($("<input/>")
+          .attr("type", "text")
+          .attr("name", "password")
+          .attr("id", "password"))
+        .append($("<input/>")
+          .attr("type", "button")
+          .attr("name", "login")
+          .attr("value", "Login")
+          .attr("id", "login"))
+      ;
+
+      return this;
+    },
+
+    login: function () {
+      var loginModel = new LoginModel({
+        user: this.$el.find("#user").val(),
+        password: this.$el.find("#password").val()
+      });
+
+      loginModel.login();
+    }
+
+  });
+
+  var WebadminRouter = Backbone.Router.extend({
+
+    routes: {
+      "": "login"
+    },
+
+    login: function () {
+      var loginForm = new LoginFormView();
+      render(loginForm.render().$el);
+    }
+
+  });
+
+  new WebadminRouter();
+  Backbone.history.start({pushState: true});
+
+});
