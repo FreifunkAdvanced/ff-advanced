ec82e04d8c6c2bbbcf4251c4908e30f02c4684c6
lcb01 <lcb01@fnordeingang.de>
2012-04-26 23:21:00 +0200

    New Network configuration * Port 1 & 2 LAN * Port 3 MESH * Port 4 Batman (for Node interconnection)

--- files/trunk/ar71xx/lib/functions/ff-defaults.sh
+++ files/trunk/ar71xx/lib/functions/ff-defaults.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 # By DocFox -> lcb01@jabber.ccc.de
 
-ffdef_set_interface_wlan() {
-	local ifname=$1
-	uci batch << EOF
-set network.wlan='interface'
-set network.wlan.ifname='$ifname'
-set network.wlan.mtu='1528'
-set network.wlan.proto='none'
-set network.wlan.auto='1'
-EOF
-}
-
 ffdef_set_interface_mesh() {
 	local ifname=$1
 	uci batch <<EOF
@@ -22,3 +11,18 @@ set network.mesh.proto='none'
 set network.mesh.auto='1'
 EOF
 }
+
+ffdef_add_batman_port {
+	local interface=$1
+	local ifname=$2
+	local batinterfaces=$(uci get batman-adv.bat0.interfaces)
+	uci batch <<EOF
+set network.$interface='interface'
+set network.$interface.ifname='$ifname'
+set network.$interface.mtu='1528'
+set network.$interface.proto='none'
+set network.$interface.auto='1'
+set batman-adv.bat0.interfaces='$batinterfaces $ifname'
+EOF
+}
+
