4832d57175715eca8c4dafc4cb5e641127007ca1
lcb01 <lcb01@fnordeingang.de>
2012-04-27 22:23:03 +0200

    Fixes to uci-defaults/network, functions/ff-defaults.sh * Batman LAN ports will now fragment packages (VLAN's dont support a MTU of 1528)

--- files/trunk/ar71xx/lib/functions/ff-defaults.sh
+++ files/trunk/ar71xx/lib/functions/ff-defaults.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 # By DocFox -> lcb01@jabber.ccc.de
 
+ffdef_set_interface_wlan() {
+	local ifname=$1
+	uci batch <<EOF
+set network.wlan='interface'
+set network.wlan.ifname='$ifname'
+set network.wlan.mtu='1528'
+set network.wlan.proto='none'
+set network.wlan.auto='1'
+EOF
+}
+
 ffdef_set_interface_mesh() {
 	local ifname=$1
 	uci batch <<EOF
@@ -12,16 +23,22 @@ set network.mesh.auto='1'
 EOF
 }
 
-ffdef_add_batman_port() {
+ffdef_set_interface_raw() {
 	local interface=$1
 	local ifname=$2
 	uci batch <<EOF
 set network.$interface='interface'
 set network.$interface.ifname='$ifname'
-set network.$interface.mtu='1528'
 set network.$interface.proto='none'
 set network.$interface.auto='1'
-add_list batman-adv.bat0.interfaces='$ifname'
+EOF
+}
+
+
+ffdef_set_interface_batman() {
+	local ifname=$1
+	uci batch <<EOF
+set batman-adv.bat0.interfaces='wlan0-1 $ifname'
 EOF
 }
 
