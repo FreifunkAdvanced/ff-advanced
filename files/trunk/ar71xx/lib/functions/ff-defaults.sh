d491b56074cf200652d29aa702b4a026eeca1272
lcb01 <lcb01@fnordeingang.de>
2012-06-08 21:30:46 +0200

    Fixed netifd related bug  * Added 'option network adhoc' to wireless configs.  * Removed ifname from wlan network in the network configs.  * Renamed network wlan to adhoc  * Patched functions/ff-defaults.sh accordingly  * Patch uci-defaults/network accordingly
    Info about bug -> https://forum.openwrt.org/viewtopic.php?id=37132

--- files/trunk/ar71xx/lib/functions/ff-defaults.sh
+++ files/trunk/ar71xx/lib/functions/ff-defaults.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # By DocFox -> lcb01@jabber.ccc.de
 
-ffdef_set_interface_wlan() {
-	local ifname=$1
+ffdef_set_interface_adhoc() {
 	uci batch <<EOF
-set network.wlan='interface'
-set network.wlan.ifname='$ifname'
-set network.wlan.mtu='1528'
-set network.wlan.proto='none'
-set network.wlan.auto='1'
+set network.adhoc='interface'
+set network.adhoc.mtu='1528'
+set network.adhoc.proto='none'
+set network.adhoc.auto='1'
 EOF
 }
 
